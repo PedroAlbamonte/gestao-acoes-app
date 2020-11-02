f306fd2155021276809746e43dcf2d8ac0a6241a
Pedro Albamonte <pedro.albamonte@livelo.com.br>
2020-11-02 20:09:41 -0300

    stickyHeader

--- src/pages/Darf/index.js
+++ src/pages/Darf/index.js
@@ -6,13 +6,21 @@ import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 import Paper from '@material-ui/core/Paper';
+import { makeStyles } from "@material-ui/core/styles";
 import DarfItem from '../../components/DarfItem';
 import api from '../../services/api';
 
 import './styles.css';
 
+const useStyles = makeStyles({
+  container: {
+    maxHeight: 700
+  }
+});
+
 export default function Darf() {
   const [darfs, setDarfs] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     async function loadDarfs() {
@@ -26,9 +34,9 @@ export default function Darf() {
 
   return (
     <div className="register-container">
-      <div className="content">
-        <TableContainer component={Paper}>
-          <Table aria-label="collapsible table" className="darf-table">
+      <div className="content" id="id-content">
+        <TableContainer component={Paper} className={classes.container}>
+          <Table aria-label="collapsible table" className="darf-table" stickyHeader>
             <TableHead>
               <TableRow>
                 <TableCell rowSpan={2} />
@@ -43,16 +51,16 @@ export default function Darf() {
                 <TableCell rowSpan={2} align="center"><strong>Total Devido</strong></TableCell>
               </TableRow>
               <TableRow>
-                <TableCell align="center"><strong>Lucro</strong></TableCell>
-                <TableCell align="center"><strong>Lucro</strong></TableCell>
-                <TableCell align="center"><strong>Saldo</strong></TableCell>
-                <TableCell align="center"><strong>IR</strong></TableCell>
-                <TableCell align="center"><strong>Lucro</strong></TableCell>
-                <TableCell align="center"><strong>Saldo</strong></TableCell>
-                <TableCell align="center"><strong>IR</strong></TableCell>
-                <TableCell align="center"><strong>Lucro</strong></TableCell>
-                <TableCell align="center"><strong>Saldo</strong></TableCell>
-                <TableCell align="center"><strong>IR</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>Lucro</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>Lucro</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>Saldo</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>IR</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>Lucro</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>Saldo</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>IR</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>Lucro</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>Saldo</strong></TableCell>
+                <TableCell align="center" style={{ top: 58 }}><strong>IR</strong></TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
